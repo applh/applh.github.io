8d7e72468b2e49ddc8ac56a5c592eab53faa2d71
Applh.com <github@applh.com>
2014-04-13 18:48:18 +0200

    add send by email

--- @/echo.php
+++ @/echo.php
@@ -3,6 +3,8 @@
 $data=trim($_REQUEST['data']);
 $enc=trim($_REQUEST['enc']);
 $md5=basename(trim($_REQUEST['md5'])); // NEEDS SOME PROTECTION HERE
+$md5=preg_replace('/[^a-zA-Z0-9]/g', "", $md5);
+
 $mode=trim($_REQUEST['mode']);
 
 if ($enc == "base64") {
@@ -15,7 +17,7 @@ if (($mode == "write") && !empty($data) && $md5) {
 	$tmpfile=__DIR__."/tmp/$md5.html";
 	file_put_contents(__DIR__."/tmp/$md5.html", $data);
 }
-else if (($mode == "read") && $md5) {
+elseif (($mode == "read") && $md5) {
 	$tmpfile=__DIR__."/tmp/$md5.html";
 	if (is_file($tmpfile)) {
 		$data=file_get_contents(__DIR__."/tmp/$md5.html");
@@ -24,4 +26,20 @@ else if (($mode == "read") && $md5) {
 		$data="NOT FOUND ".date("H:i:s");
 	}
 	echo $data;
+}
+elseif (($mode == "email") && !empty($data) && $md5) {
+	$tmpfile=__DIR__."/tmp/$md5.html";
+	file_put_contents(__DIR__."/tmp/$md5.html", $data);
+        $to=trim(base64_decode($_REQUEST['email']));
+        if ($to && $data) {
+            $subject="Cahier Des Charges";
+            $content=$data;
+            $tab2headers=array(
+                  "from: no-reply@frimi.fr", 
+                  "Content-Type: text/html",
+                  );
+            $headers=implode("\r\n", $tab2headers);
+
+            mail($to, $subject, $content, $headers);
+        }
 }
